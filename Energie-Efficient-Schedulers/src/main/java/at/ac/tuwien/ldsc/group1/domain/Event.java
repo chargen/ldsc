693a9987288639d0e1787f6ffa6108573e0f3772
Sebastian Geiger <sbastig@gmx.net>
2013-04-21 12:19:37 +0200

    Event: Use long instead of int for timestamp

--- Energie-Efficient-Schedulers/src/main/java/at/ac/tuwien/ldsc/group1/domain/Event.java
+++ Energie-Efficient-Schedulers/src/main/java/at/ac/tuwien/ldsc/group1/domain/Event.java
@@ -1,17 +1,17 @@
 package at.ac.tuwien.ldsc.group1.domain;
 
 public class Event {
-    int eventTime; // time on the event time line.
+    long eventTime; // time on the event time line.
     EventType eventType;
     Application application;
 
-    public Event(int eventTime, EventType eventType, Application application) {
+    public Event(long eventTime, EventType eventType, Application application) {
         this.eventTime = eventTime;
         this.eventType = eventType;
         this.application = application;
     }
 
-    public int getEventTime() {
+    public long getEventTime() {
         return eventTime;
     }
     public EventType getEventType() {
