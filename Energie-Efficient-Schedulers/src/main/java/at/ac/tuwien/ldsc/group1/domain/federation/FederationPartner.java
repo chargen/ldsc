142b70adc23a6a5efc9e9b0bb6d1386a8152a4fd
Patonai <patonaipeter@gmail.com>
2013-06-12 14:12:39 +0200

    FederationPartner

---
+++ Energie-Efficient-Schedulers/src/main/java/at/ac/tuwien/ldsc/group1/domain/federation/FederationPartner.java
@@ -0,0 +1,15 @@
+package at.ac.tuwien.ldsc.group1.domain.federation;
+
+import at.ac.tuwien.ldsc.group1.domain.components.Application;
+
+public class FederationPartner {
+	
+	
+	public Application getSourceInApplication(ScenarioType type){
+		return null;
+	}
+	public boolean deploySourceOutApplication(Application app){
+		return true;
+	}
+
+}
---
+++ Energie-Efficient-Schedulers/src/main/java/at/ac/tuwien/ldsc/group1/domain/federation/ScenarioType.java
@@ -0,0 +1,7 @@
+package at.ac.tuwien.ldsc.group1.domain.federation;
+
+public enum ScenarioType {
+
+	WBA, HPA, MIXED;
+	
+}
